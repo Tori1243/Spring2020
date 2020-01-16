1b6a3c747903ae503b2ebc64b02b5ec0d54c557d
Unknown <Tori7809@gmail.com>
2020-01-15 20:08:52 -0700

    destroy and Move scripts
    The only two scripts I actually know how to write :)

---
+++ Review/Assets/Scripts that Work/Destroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DestroyObject : MonoBehaviour
+{
+    public UnityEvent ClickEvent;
+
+    private void OnMouseDown()
+    {
+        ClickEvent.Invoke();
+        Destroy(gameObject);
+    }
+}

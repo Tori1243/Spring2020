af1e2b8c652066c79b906214465e6b776ccfe512
Unknown <Tori7809@gmail.com>
2020-01-17 14:05:47 -0700

    Attack Script
    This does nothing yet

---
+++ Review/Assets/WIPScripts/Attack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject Player;
+ 
+
+    public void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Health>();
+        {
+            Destroy(Player.gameObject);
+        }
+
+    }
+}
+ 
+ 
---
+++ Review/Assets/WIPScripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour {
+    public UnityEvent DeathEvent;
+
+    public int health = 100;
+    public void ChangeHealth(int NewHealth){
+        health = health + NewHealth;
+        KillPlayer();
+    
+    }
+    public void KillPlayer()
+    {
+        if (health < 0 && gameObject.tag == "Player")
+        {
+            DeathEvent.Invoke();
+        }
+        else
+        {
+            return;
+        }
+    }
+
+}

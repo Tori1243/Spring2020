6cf41e0953e38df4fe7aa4cb27d65031efe99511
Unknown <Tori7809@gmail.com>
2020-01-16 21:51:35 -0700

    Collison

---
+++ Review/Assets/Scripts that Work/Collison.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Collison : MonoBehaviour
+{
+
+	public UnityEvent CollisionEnterEvent;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		CollisionEnterEvent.Invoke();
+	}
+
+}

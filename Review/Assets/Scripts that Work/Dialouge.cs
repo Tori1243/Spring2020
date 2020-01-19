995beec4f0c475def6e122c2fb71d4ae778a02bf
Unknown <Tori7809@gmail.com>
2020-01-18 21:21:44 -0700

    Dialouge and follow cursor

---
+++ Review/Assets/Scripts that Work/Dialouge.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class DialogueS : MonoBehaviour
+{
+
+
+    public UnityEvent EndOfTextEvent;
+    
+    //Call when DialogueBox is opened
+    
+    public void OpenBox()
+    {
+        GetComponent<CanvasGroup>().alpha = 1;
+        GetComponent<CanvasGroup>().interactable = true;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+    
+    // call when next button is pressed
+    
+    public void CloseBox()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+        GetComponent<CanvasGroup>().interactable = false;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        EndOfTextEvent.Invoke();
+    }
+    
+    
+}

e5c3c971f075ce3984ec793de64a22a16885cca2
Unknown <Tori7809@gmail.com>
2020-01-21 18:20:04 -0700

    Collection and Storage

---
+++ Review/Assets/WIPScripts/Storage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Storage :ScriptableObject
+{
+    public ObjectCollection Collection;
+	
+    public void Call(ScriptableObject obj)
+    {
+        Collection.ObjectList.Add(obj);
+    }
+}

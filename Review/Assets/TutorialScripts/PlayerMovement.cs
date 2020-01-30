252e03b445d6a2d2eb39a6d41f7d7aa6661cdad7
Unknown <Tori7809@gmail.com>
2020-01-29 23:44:05 -0700

    player movement
    brackeys

---
+++ Review/Assets/TutorialScripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 200);
+    }
+}

735eb64ab72fe9ad061cfb1da7509b752d458795
Unknown <Tori7809@gmail.com>
2020-01-29 23:47:57 -0700

    broken. doesn't work.

--- Review/Assets/TutorialScripts/PlayerMovement.cs
+++ Review/Assets/TutorialScripts/PlayerMovement.cs
@@ -4,10 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-
-    // Update is called once per frame
     void Update()
     {
-        rb.AddForce(0, 0, 200);
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
 }

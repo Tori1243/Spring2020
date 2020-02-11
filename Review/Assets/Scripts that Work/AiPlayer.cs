61006c05d06b35a45cfcd07634748fa34ec60b98
Unknown <Tori7809@gmail.com>
2020-02-11 07:04:16 -0700

    LightChanger Script

---
+++ Review/Assets/Scripts that Work/AiPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiPlayer : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform Player, destination;
+    private void Start()
+    {
+        destination = Player;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    private void Update()
+    {
+        agent.destination = destination.position;
+        destination = Player;
+    }
+}

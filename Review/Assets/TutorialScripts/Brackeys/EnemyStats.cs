50440973b50504f6f424d7c40afd32af74eceb99
Unknown <Tori7809@gmail.com>
2020-02-14 11:31:31 -0700

    EnemyStats

---
+++ Review/Assets/TutorialScripts/Brackeys/EnemyStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : CharStats
+{
+    public Color friendColor = Color.green;
+    //public ClickParticles enemyParticles;
+    
+    public override void Die()
+    {
+        base.Die();
+        
+        gameObject.GetComponent<Renderer>().material.color = friendColor;
+        
+        Destroy(gameObject);
+        // enemyParticles.ghostPlasm.emissionRate = 0f;
+
+    }
+
+    /*public void Update()
+    {
+         if (Input.GetMouseButtonDown(0))
+         {
+             TakeDamage(5);
+         }
+    }*/
+}

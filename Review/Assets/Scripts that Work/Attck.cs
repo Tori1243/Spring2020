c02277ca18aa2d657361bb1fcfbbbfd05e37861e
Unknown <Tori7809@gmail.com>
2020-02-19 13:57:34 -0700

    Attacking and new player mover script

--- Review/Assets/Scripts that Work/Attck.cs
+++ Review/Assets/Scripts that Work/Attck.cs
@@ -1,11 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Attck : MonoBehaviour {
-    public int AttackAmount;
-    public void OnTriggerEnter(Collider other)
+public class Attck : MonoBehaviour
+{
+    public GameObject Hitbox;
+    public float Timer;
+    public bool AttacksIsRunning = false;
+   
+    void Update()
     {
-        print("attacking");
-        if (other.GetComponent<Health>())
-            other.GetComponent<Health>().ChangeHealth(AttackAmount);
+        //Get input
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (AttacksIsRunning == false)
+            {
+                StartCoroutine(Attacks());
+            }
+            
+        }
+    }
+
+    IEnumerator Attacks()
+    {
+        AttacksIsRunning = true;
+        //Turn on Hit box
+        Hitbox.SetActive(true);
+        
+        //Set Timer
+        yield return new WaitForSeconds(Timer);
+        
+        //Turn off
+        Hitbox.SetActive(false);
+
+        AttacksIsRunning = false;
     }
 }
---
+++ Review/Assets/Scripts that Work/PlayerMover.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerMover : MonoBehaviour
+{
+    private Vector3 position;
+    //private Vector3 rotation;
+    private CharacterController controller;
+    
+    public float moveSpeed = 300f, gravity = 9.81f, jumpSpeed = 30f;
+    private int jumpCount;
+    public int jumpCountMax = 2;
+    public float SprintSpeed = 600f;
+    public float WalkSpeed = 300f;
+    public float RotationSpeed = 500f;
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input for running
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            // Changing speed variable
+            moveSpeed = SprintSpeed; 
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = WalkSpeed;
+        }
+		
+        // get input to stop running
+        //Change speed variable
+        position.x = moveSpeed*Input.GetAxis("Horizontal");
+        position.z = moveSpeed*Input.GetAxis("Vertical");
+        position.y -= gravity;
+
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+            //transform.Rotate(rotation);
+        }
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+        
+        controller.Move(position*Time.deltaTime);
+    }
+}

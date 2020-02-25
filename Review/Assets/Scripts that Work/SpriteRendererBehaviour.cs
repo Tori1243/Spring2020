51c7f43a7adbc75b56bdfac0b9af963738ad6d46
Unknown <Tori7809@gmail.com>
2020-02-25 12:40:42 -0700

    Sprite Renderer

---
+++ Review/Assets/Scripts that Work/SpriteRendererBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteRendererBehaviour : MonoBehaviour
+{
+    private SpriteRenderer spriteRendererObj;
+    public ColorData colorDataObj;
+    public UnityEvent startEvent;
+
+    void Start()
+    {
+        spriteRendererObj.GetComponent<SpriteRenderer>();
+        startEvent.Invoke();
+        
+    }
+
+    public void ChangeSpriteColor(ColorData colorDataObj)
+    {
+        spriteRendererObj.color = colorDataObj.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

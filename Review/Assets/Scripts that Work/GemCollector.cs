54adb0e49f8c3b3a34a4e4ce6e591dd0658044b4
Unknown <Tori7809@gmail.com>
2020-02-25 12:34:48 -0700

    broken broken broken scripts

---
+++ Review/Assets/Scripts that Work/GemCollector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class GemCollector : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+
+    public List<GemCollector> coinList;
+
+
+    public void AddCoin(GemCollector coinObj)
+    {
+        if (!coinList.Contains(coinObj))
+        {
+            coinList.Add(coinObj);
+        }
+    }
+
+    public void UseCoin()
+    {
+        coinList[0].Spend();
+        coinList.RemoveAt(0);
+    }
+
+    public void UseCoinWithValue(int value)
+    {
+        var number = 5;
+        for (var i = 0; i < coinList.Count; i++)
+        {
+            var coin = coinList[i];
+            if (coin.value > number)
+            {
+                coin.Spend();
+                coinList.Remove(coin);
+            }
+        }
+    }
+
+    public void UseAllCoins()
+    {
+        foreach (var coin in coinList)
+        {
+            coin.Spend();
+        }
+
+        coinList.Clear();
+    }
+
+    public void UpgradeAllCoins(int value)
+    {
+        foreach (var coin in coinList)
+        {
+            coin.value = value;
+        }
+    }
+}

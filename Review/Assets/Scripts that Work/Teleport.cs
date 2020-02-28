d7a5a27a77b601e31bd705334844d8a6db56a1ab
Unknown <Tori7809@gmail.com>
2020-02-28 13:17:19 -0700

    Teleport script

---
+++ Review/Assets/Scripts that Work/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour
+
+{
+    public GameObject TeleportSpot;
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+        print(TeleportSpot.transform.position);
+        other.GetComponent<CharacterController>().enabled = false;
+       other.gameObject.transform.position = TeleportSpot.transform.localPosition;
+        other.GetComponent< CharacterController>().enabled = true;
+        //other.gameObject.transform.position = new Vector3(0, 0, 0);
+
+    }
+}

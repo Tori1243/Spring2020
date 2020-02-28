afd060aee48b27a5e8b02336cc8d35e0c039f216
Unknown <Tori7809@gmail.com>
2020-02-28 13:20:51 -0700

    Attack Scripts

---
+++ Review/Assets/TutorialScripts/AttackEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttacks : MonoBehaviour
+{
+    public GameObject Hitbox;
+    public float Timer;
+    public bool AttacksIsRunning = false;
+   
+    void OnTriggerEnter()
+    {
+       
+        if (AttacksIsRunning == false)
+        {
+            StartCoroutine(Attacks());
+        }
+            
+    }
+
+    IEnumerator Attacks()
+    {
+        AttacksIsRunning = true;
+        //Turn on Hit box
+        Hitbox.SetActive(true);
+        
+        //Set Timer
+        yield return new WaitForSeconds(Timer);
+        
+        //Turn off
+        Hitbox.SetActive(false);
+
+        AttacksIsRunning = false;
+    }
+}

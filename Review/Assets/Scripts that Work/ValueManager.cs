5a9f703a2acc5177152c06b20e0231a2daaf27ca
Unknown <Tori7809@gmail.com>
2020-03-03 11:52:03 -0700

    value scripts

---
+++ Review/Assets/Scripts that Work/ValueManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+
+public class ValueManager : MonoBehaviour
+{
+    public FloatData GemTotal;
+
+    public void ChangeGemTotal(float delta)
+    {
+        GemTotal.Value += delta;
+    }
+
+

5bb9338edcecb251c6de28748369372adb98f644
Unknown <Tori7809@gmail.com>
2020-03-10 12:38:23 -0600

    resources and Collections

---
+++ Review/Assets/WIPScripts/Collection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class Collection : ScriptableObject
+{
+    public List<Collectable> CollectableList;
+
+    public void AddToCollections(Collectable collectableObj)
+    {
+        CollectableList.Add(collectableObj);
+    }
+
+    public void RemoveFromCollection(Collectable collectableObj)
+    {
+        for (var index = CollectableList.Count - 1; index >= 0; index--)
+        {
+            var obj = CollectableList[index];
+            if (obj == collectableObj)
+            {
+                CollectableList.Remove(collectableObj);
+            }
+        }
+    }
+
+    public void ClearCollection()
+    {
+        CollectableList.Clear();
+    }
+    
+}
---
+++ Review/Assets/WIPScripts/OnMouseEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnMouseEvent : MonoBehaviour
+{
+    public UnityEvent OnMouseDownEvent;
+
+    private void OnMouseDown()
+    {
+        OnMouseDownEvent.Invoke();
+    }
+
+    public UnityEvent OnMouseUpEvent;
+
+    private void OnMouseUp()
+    {
+        OnMouseUpEvent.Invoke();
+    }
+
+}

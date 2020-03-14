e926929d4e5efe177518919df9d8dedfe5dea992
Unknown <Tori7809@gmail.com>
2020-03-13 23:32:08 -0600

    lightChanger script update

--- Review/Assets/Scripts that Work/LightChanger.cs
+++ Review/Assets/Scripts that Work/LightChanger.cs
@@ -4,18 +4,24 @@ public class LightChanger : MonoBehaviour {
 
     public GameObject light;
     private bool on = false;
+    public int BallLightNumber;
+    public CheckForLight checkForLight;
 
     // Use this for initialization
     void OnTriggerStay(Collider plyr) {
         if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !on)
         {
             light.SetActive(true);
+            checkForLight.CheckLightOn(BallLightNumber);
             on = true;
+            
         }
         else if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && on)
-            {
+        {
             light.SetActive(false);
             on = false;
         }
+        
     }
+    
 }

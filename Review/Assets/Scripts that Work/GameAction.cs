84d3f7e6449ad449be91ea88744889612adf2fa7
Unknown <Tori7809@gmail.com>
2020-03-26 13:07:04 -0600

    Ai Hunt, addpoints, Idle, patrol

---
+++ Review/Assets/Scripts that Work/GameAction.cs
@@ -0,0 +1,23 @@
+﻿  
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Basics
+{
+    [CreateAssetMenu(menuName = "Actions/GameAction")]
+    public class GameAction: ScriptableObject
+    {
+        public UnityAction<object> Raise;
+
+        public void OnRaise(float obj)
+        {
+            Raise(obj);
+        }
+
+        public void raise(List<Vector3Data> points)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

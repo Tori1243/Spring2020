cd401f94d36682fc43d19f4d39a9fafad7b72d1c
Unknown <Tori7809@gmail.com>
2020-03-27 23:42:49 -0600

    I just broke everything

---
+++ Review/Assets/Scripts that Work/TeleportSpot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TeleportSpot : MonoBehaviour
+
+{
+    public GameObject TeleportSpot;
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+        print(TeleportSpot.transform.position);
+        
+        other.GetComponent<CharacterController>().enabled = false;
+        
+        other.gameObject.transform.position = TeleportSpot.transform.localPosition;
+        
+        other.GetComponent< CharacterController>().enabled = true;
+       
+
+    }
+}

44df825504d150dc608fdfec90bbbddf1d4a6025
Unknown <Tori7809@gmail.com>
2020-03-30 18:38:08 -0600

    Old StateMachine Script

---
+++ Review/Assets/Scripts that Work/StateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StateMachine : MonoBehaviour {
+    public string GameState;
+    public UnityEvent Starting, Playing, Ending, Dying;
+
+	void Start () 
+        {
+        Run();
+        }
+    public void ChangeState(string NewState)
+        {
+        print(NewState);
+           GameState = NewState;
+            Run();
+        }
+	
+	
+	  
+    public void Run() {
+       switch (GameState)
+        {
+            case "Starting":
+               Starting.Invoke();
+                break;
+
+            case "Dying":
+               Dying.Invoke();
+                break;
+
+            case "Playing":
+               Playing.Invoke();
+                break;
+
+            case "Ending":
+                Ending.Invoke();
+                break;
+        }
+    }
+		
+}

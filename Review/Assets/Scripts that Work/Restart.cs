523e422a72e9acd6091876e7d8ddb28128c90785
Unknown <Tori7809@gmail.com>
2020-04-02 22:49:30 -0600

    ReSpawn script

---
+++ Review/Assets/Scripts that Work/Restart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Restart : MonoBehaviour
+
+{
+    public GameObject SpawnSpot;
+    public UnityEvent DeadEvent;
+        
+    
+    public void ReturnToSpawn()
+    {
+        // spawn to spawn point
+        GetComponent<CharacterController>().enabled = false;
+        transform.position = SpawnSpot.transform.position;
+        GetComponent<CharacterController>().enabled = true;
+        GetComponent<Health>().HealthStorage = 100;
+        
+    DeadEvent.Invoke();
+    }
+    
+    
+    //not destroy himself
+    //restore health to full
+}

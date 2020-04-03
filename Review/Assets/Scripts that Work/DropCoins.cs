2144908e6b2f2749cb6a9e3e7b8d6c919342474a
Unknown <Tori7809@gmail.com>
2020-04-02 22:48:27 -0600

    DropCoins script

---
+++ Review/Assets/Scripts that Work/DropCoins.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCoins : MonoBehaviour
+{
+    public GameObject coin;
+
+    public void InstantiateOnEnemy()
+   
+    {
+        Instantiate(coin, transform.position, Quaternion.identity);
+    }
+}

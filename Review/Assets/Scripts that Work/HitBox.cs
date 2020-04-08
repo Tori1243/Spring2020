54a45e44a4eae99daceeee8ac01455fb0e8d5a88
Unknown <Tori7809@gmail.com>
2020-04-08 11:46:13 -0600

    HitBox

---
+++ Review/Assets/Scripts that Work/HitBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        print(other.gameObject);
+        if (other.GetComponent<Health>())
+        {
+            other.GetComponent<Health>().takeHealth();
+            print("Hitting Something");
+        }
+
+        if (other.GetComponent<KnockBack>())
+        {
+            other.GetComponent<KnockBack>().attacker = transform.parent.parent.gameObject;
+            other.GetComponent<KnockBack>().OnHit();
+
+        }
+
+    }

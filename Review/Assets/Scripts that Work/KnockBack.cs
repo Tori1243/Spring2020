ccc604a3543e37d803bfe3410a8d7b8afadb6a5f
Unknown <Tori7809@gmail.com>
2020-04-07 23:38:16 -0600

    hit player back after attack

---
+++ Review/Assets/Scripts that Work/KnockBack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+
+{
+    public float KnockBackSpeed;
+    public GameObject attacker;
+    public void OnHit()
+    {
+        Vector3 direction = attacker.transform.position - transform.position;
+
+        GetComponent<CharacterController>().Move(direction * KnockBackSpeed);
+
+    }
+   // direction = ai.position - player.position;
+
+  // distance = 1; destination = direction * distance
+}

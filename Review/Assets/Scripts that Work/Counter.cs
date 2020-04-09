6ef7e496ddf54a187528775b094636fc1dbba9f2
Unknown <Tori7809@gmail.com>
2020-04-09 11:30:35 -0600

    Count Down

---
+++ Review/Assets/Scripts that Work/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter: MonoBehaviour {
+
+    private Text counterLabel;
+
+	// Use this for initialization
+    IEnumerator Start () {
+        counterLabel = GetComponent<Text>();
+        counterLabel.text = "3";
+        yield return new WaitForSeconds(1);
+        counterLabel.text = "2";
+        yield return new WaitForSeconds(1);
+        counterLabel.text = "1";
+        yield return new WaitForSeconds(1);
+        counterLabel.text = "GO!";
+        yield return new WaitForSeconds(1);
+        counterLabel.text = "";
+
+    }
+	
+}

6b981c4b92468886cf2500a663e6ab0de11855bd
Unknown <Tori7809@gmail.com>
2020-04-19 17:55:39 -0600

    Add value

--- Review/Assets/Scripts that Work/AddValue.cs
+++ Review/Assets/Scripts that Work/AddValue.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class AddValue : MonoBehaviour
+[CreateAssetMenu]
+public class AddValue : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public FloatData ValueObj;
+    public FloatData MaxValue;
+    public FloatData MinValue;
 
-    // Update is called once per frame
-    void Update()
+    public UnityEvent EventMax;
+    public UnityEvent EventMin;
+
+    public void AddValueToObj(FloatData data)
     {
-        
+        ValueObj.Value += data.Value;
+
+        if (ValueObj.Value >= MaxValue.Value)
+        {
+            EventMax.Invoke();
+        }
+
+        if (ValueObj.Value <= MinValue.Value) 
+        {
+            EventMin.Invoke();
+        }
     }
 }

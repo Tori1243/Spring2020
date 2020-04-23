4dfad02064b898c6acbcba4f5e7a874e95294f33
Unknown <Tori7809@gmail.com>
2020-04-23 15:41:42 -0600

    Interacting

---
+++ Review/Assets/Scripts that Work/InteractScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractScript : MonoBehaviour
+{
+    public bool canInteract = false;
+
+    public GameObject interactableObj;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Interact")
+        {
+           
+            interactableObj = other.gameObject;
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Interact")
+        {
+          
+            canInteract = false;
+        }
+    }
+    //hello
+    public void Update()
+    {
+        if (canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            interactableObj.GetComponent<InteractEvent>().interactEvent.Invoke();
+        }
+    }
+}

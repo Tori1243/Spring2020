20ff31b07e15819e5c1c6152e954641afd360ea7
Unknown <Tori7809@gmail.com>
2020-04-30 10:12:15 -0600

    Shield

---
+++ Review/Assets/Scripts that Work/Shield.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public bool Shielding; 
+    public GameObject ShieldObject;
+    public int ShieldHealth;
+
+    
+    void Update()
+    {
+        //Get input for shielding
+        if(Input.GetMouseButtonDown(1))
+        {
+            //Change shield variable
+            Shielding = true;
+            ShieldObject.SetActive(true);
+            GetComponent<Health>().takeHealth(ShieldHealth);
+        }
+    
+        //Get input for stop shielding
+        if(Input.GetMouseButtonUp(1))
+        {
+            //return shield variable to normal
+            Shielding = false;
+            ShieldObject.SetActive(false);
+            
+        }
+                   
+    }
+    
+}
